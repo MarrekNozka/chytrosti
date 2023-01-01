a971b468dfcec9d60bfbc0d106b0b5b1192c83dc
Marrek Nožka <marek@tlapicka.net>
2023-01-01 17:30:34 +0100

    Publish site on 2023-01-01

--- theme/js/videopreview.js
+++ theme/js/videopreview.js
@@ -1,36 +1,38 @@
-var img;
-var frames;
-var imgsrc;
-var width;
-var _width;
-var _height;
-var imgalt;
-var nth;
+// video-preview
 
-var links = document.getElementsByClassName('video-preview');
-for (let i = 0, len = links.length; i < len; i++) {
-    var element = links[i];
-    img = element.getElementsByTagName('IMG')[0];
-    imgsrc = img.src
-    imgalt = img.alt
-    frames = img.dataset.frames;
-    _width = img.naturalWidth;
-    _height = img.naturalHeight;
-    width = _width/frames;
+let videolinks = document.getElementsByClassName('video-preview');
+
+for (let i = 0; i < videolinks.length; i++) {
+
+    let element = videolinks[i];
+    let img = element.getElementsByTagName('IMG')[0];
+    let imgsrc = img.src
+    let imgalt = img.alt
+    let frames = img.dataset.frames;
+    let _width = img.naturalWidth;
+    let _height = img.naturalHeight;
+    let width = _width/frames;
     element.style.width = width + 'px';
     element.style.height = _height + 'px';
-    console.log(_width, _height, width)
-    console.log(element.style.width)
-    console.log(element.style.height)
+
+    //console.log(_width, _height, width)
+    //console.log(element.style.width)
+    //console.log(element.style.height)
     
     element.innerHTML = '<span>▷ '+imgalt+'</span>'
     element.style.backgroundImage = 'url("'+imgsrc+'")'
-    links[i].addEventListener('mousemove', mouseMove)
+
+    element.addEventListener('mousemove', mouseMove)
+    element.frames = frames;
+    element.frames = frames;
+    element.width = width;
 }
 
 function mouseMove(event) {
-    var x = event.offsetX;
-    var step = parseInt(width/frames)
-    var nth = parseInt(x / step)
+    let x = event.offsetX;
+    let width = this.width;
+    let frames = this.frames
+    let step = parseInt(width / frames)
+    let nth = parseInt(x / step)
     this.style.backgroundPosition = (-width * nth) + 'px';
 }
