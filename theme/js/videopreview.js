9d2e12145e8ea698bc36fe3eb8d46d449aac12c9
Marrek Nožka <marek@tlapicka.net>
2023-11-29 15:36:42 +0100

    Publish site on 2023-11-29

--- theme/js/videopreview.js
+++ theme/js/videopreview.js
@@ -1,33 +1,4 @@
 // video-preview
-
-let videolinks = document.getElementsByClassName('video-preview');
-
-for (let i = 0; i < videolinks.length; i++) {
-
-    let element = videolinks[i];
-    let img = element.getElementsByTagName('IMG')[0];
-    let imgsrc = img.src
-    let imgalt = img.alt
-    let frames = img.dataset.frames;
-    let _width = img.naturalWidth;
-    let _height = img.naturalHeight;
-    let width = _width/frames;
-    element.style.width = width + 'px';
-    element.style.height = _height + 'px';
-
-    //console.log(_width, _height, width)
-    //console.log(element.style.width)
-    //console.log(element.style.height)
-    
-    element.innerHTML = '<span>▷ '+imgalt+'</span>'
-    element.style.backgroundImage = 'url("'+imgsrc+'")'
-
-    element.addEventListener('mousemove', mouseMove)
-    element.frames = frames;
-    element.frames = frames;
-    element.width = width;
-}
-
 function mouseMove(event) {
     let x = event.offsetX;
     let width = this.width;
@@ -36,3 +7,36 @@ function mouseMove(event) {
     let nth = parseInt(x / step)
     this.style.backgroundPosition = (-width * nth) + 'px';
 }
+
+function video_preview_create(event) {
+    let videolinks = document.getElementsByClassName('video-preview');
+
+    for (let i = 0; i < videolinks.length; i++) {
+
+        let element = videolinks[i];
+        let img = element.getElementsByTagName('IMG')[0];
+        let imgsrc = img.src
+        let imgalt = img.alt
+        let frames = img.dataset.frames;
+        let _width = img.naturalWidth;
+        let _height = img.naturalHeight;
+        let width = _width/frames;
+        element.style.width = width + 'px';
+        element.style.height = _height + 'px';
+
+        //console.log(_width, _height, width)
+        //console.log(element.style.width)
+        //console.log(element.style.height)
+        
+        element.innerHTML = '<span>▷ '+imgalt+'</span>'
+        element.style.backgroundImage = 'url("'+imgsrc+'")'
+
+        element.addEventListener('mousemove', mouseMove)
+        element.frames = frames;
+        element.frames = frames;
+        element.width = width;
+    }
+}
+
+$(document).ready(video_preview_create)
+
