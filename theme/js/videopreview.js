d944523fc7da4e30b276103f7fbb5837b6ba3f05
Marrek Nožka <marek@tlapicka.net>
2023-12-03 15:45:46 +0100

    Publish site on 2023-12-03

--- theme/js/videopreview.js
+++ theme/js/videopreview.js
@@ -1,4 +1,5 @@
 // video-preview
+
 function mouseMove(event) {
     let x = event.offsetX;
     let width = this.width;
@@ -8,35 +9,33 @@ function mouseMove(event) {
     this.style.backgroundPosition = (-width * nth) + 'px';
 }
 
-function video_preview_create(event) {
-    let videolinks = document.getElementsByClassName('video-preview');
+function video_preview_create() {
+    let videoparagraphs = document.getElementsByClassName('video-preview');
 
-    for (let i = 0; i < videolinks.length; i++) {
+    for (let i = 0; i < videoparagraphs.length; i++) {
+        let paragraph = videoparagraphs[i];
+        let frames = paragraph.dataset.frames;
+        let a = paragraph.getElementsByTagName('A')[0];
+        a.frames = frames;
+        let img = paragraph.getElementsByTagName('IMG')[0];
+        let src = img.src;
+        a.addEventListener('mousemove', mouseMove)
+        let text = a.innerHTML
+        a.innerHTML = '<span id="title">▷ ' + text + '</span>'
 
-        let element = videolinks[i];
-        let img = element.getElementsByTagName('IMG')[0];
-        let imgsrc = img.src
-        let imgalt = img.alt
-        let frames = img.dataset.frames;
-        let _width = img.naturalWidth;
-        let _height = img.naturalHeight;
-        let width = _width/frames;
-        element.style.width = width + 'px';
-        element.style.height = _height + 'px';
-
-        //console.log(_width, _height, width)
-        //console.log(element.style.width)
-        //console.log(element.style.height)
-        
-        element.innerHTML = '<span>▷ '+imgalt+'</span>'
-        element.style.backgroundImage = 'url("'+imgsrc+'")'
-
-        element.addEventListener('mousemove', mouseMove)
-        element.frames = frames;
-        element.frames = frames;
-        element.width = width;
+        img.remove();
+        img = new Image();
+        img.a = a;
+        img.onload = function () {
+            const _width = this.naturalWidth;
+            const _height = this.naturalHeight;
+            let width = _width / frames;
+            this.a.style.width = width + 'px';
+            this.a.style.height = _height + 'px';
+            this.a.width = width;
+        };
+        img.src = src;
+        a.style.backgroundImage = 'url("' + src + '")'
     }
 }
-
-$(document).ready(video_preview_create)
-
+document.addEventListener('DOMContentLoaded', video_preview_create);
